c361e1a36ca2b82d56205d0130fdb82f552bd822
Amina Wolff <a.wolff@campus.tu-berlin.de>
2025-05-28 18:38:35 +0200

    profile bisschen angefangen

--- rezeptapp/app/dashboard.py
+++ rezeptapp/app/dashboard.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user #für die profilansicht
+
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
@@ -7,3 +9,8 @@ def dashboard():
     return render_template('dashboard.html')
 
 
+
+@dashboard_bp.route('/profil')
+@login_required
+def profil():
+    return render_template('profil.html', user=current_user)
