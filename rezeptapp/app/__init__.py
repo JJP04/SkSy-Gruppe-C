5fac743470dcb0604b1e0822cce2a845dc88f418
Julian Pokorny <julian.pokorny@tu-berlin.de>
2025-06-07 18:43:58 +0200

    Images on Dashboard

--- rezeptapp/app/__init__.py
+++ rezeptapp/app/__init__.py
@@ -4,6 +4,8 @@ from .extensions import db
 from .models import User
 from flask_login import LoginManager
 from transformers import pipeline
+from .models import User, Recipe
+
 
 def create_app():
     # Absoluter Pfad zum aktuellen Verzeichnis (app/)
@@ -21,17 +23,17 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.init_app(app)
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    #erstes Mal starten dauert, da Model lokal heruntergeladen wird (Achtung, ~ 1 GB)
+    # erstes Mal starten dauert, da Model lokal heruntergeladen wird (Achtung, ~ 1 GB)
     app.ner_pipeline = pipeline(
         "ner",
         model="edwardjross/xlm-roberta-base-finetuned-recipe-all",
         tokenizer="edwardjross/xlm-roberta-base-finetuned-recipe-all",
         aggregation_strategy="simple"
-
 
     )
 
@@ -39,11 +41,13 @@ def create_app():
     from .dashboard import dashboard_bp
     from .recipe import recipe_bp
 
+
     app.register_blueprint(auth_bp)
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(recipe_bp)
 
     with app.app_context():
+        #Recipe.__table__.drop(db.engine, checkfirst=True)  #Tabelle löschen
         db.create_all()
 
     return app
